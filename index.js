5799494a40688966ccc78870cc5ac2052a6a1224
Jake Moretz <jmoretz9090@gmail.com>
2023-01-11 22:29:57 -0500

    working Etch-a-Sketch, need more styles

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
-const container = document.querySelector('.container');
-const grid = {rows:16, cols:16};
-const total = grid.rows * grid.cols;
+let color = 'black';
+let click = true
 
-createGrid(total);
+function populateBoard(size) {
+    let board = document.querySelector('.board')
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
 
-function createGrid(total) {
-    for ( let i = 0; i < total; i++){
-        const cell = document.createElement("div");
-        container.append(cell);
-        cell.textContent = `${i+1}`;
-        cell.classList.add('box');
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', colorSquare);
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend', square);
     }
-    
 }
 
-document.styleSheets[0].insertRule('.box:hover {background-color: black}');
+populateBoard(16);
+
+
+function changeSize(input) {
+    if(input >= 2  && input <= 100) {
+        document.querySelector('.error').style.display = 'none';
+        populateBoard(input);
+    } else {
+        document.querySelector('.error').style.display = 'flex';
+    }
+}
+
+function colorSquare() {
+    if (click) {
+        if (color === 'random') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;       
+        } else {
+            this.style.backgroundColor = color;
+        }
+    }
+}
+
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard () {
+    let board = document.querySelector('.board')
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+document.querySelector('body').addEventListener('click', (e) => {
+    if(e.target.tagName != 'BUTTON') {
+        click = !click;
+        if (click) {
+            document.querySelector('.mode').textContent = 'Mode: Coloring';
+        } else {
+            document.querySelector('.mode').textContent = 'Mode: Not Coloring';
+        }
+    }   
+})
